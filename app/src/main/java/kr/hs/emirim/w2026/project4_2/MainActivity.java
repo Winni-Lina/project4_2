d88a89f6983499b2a56bfcf1c3b47b78dbb648a5
Winni_Lina <9811011sa01@gmail.com>
2021-04-15 19:46:34 +0900

    직접풀어보기 4-4 종료, 처음으로 버튼 2개 추가

--- app/src/main/java/kr/hs/emirim/w2026/project4_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2026/project4_2/MainActivity.java
@@ -5,13 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.Switch;
-import android.widget.Switch;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Switch switchStart;
@@ -27,12 +26,34 @@ public class MainActivity extends AppCompatActivity {
         linear = findViewById(R.id.linear);
         rg = findViewById(R.id.rg);
         imgv = findViewById(R.id.imgv);
-        Button btnDone = findViewById(R.id.btn_done);
+        Button btnFinish = findViewById(R.id.btn_finish);
+        Button btnFirst = findViewById(R.id.btn_first);
+
+        btnFinish.setOnClickListener(btnListener);
+        btnFirst.setOnClickListener(btnListener);
 
         switchStart.setOnCheckedChangeListener(checkListener);
-        btnDone.setOnClickListener(btnListener);
+        rg.setOnCheckedChangeListener(rgListener);
 
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_finish :
+                    finish();
+                    break;
+                case R.id.btn_first:
+                    if (switchStart.isChecked())
+                        linear.setVisibility(View.VISIBLE);
+                    else
+                        linear.setVisibility(View.INVISIBLE);
+                        switchStart.setChecked(false);
+                    break;
+                }
+            }
+        };
 
     CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
@@ -46,10 +67,10 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
-    View.OnClickListener btnListener = new View.OnClickListener() {
+    RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onClick(View v) {
-            switch(rg.getCheckedRadioButtonId()){
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch(checkedId){
                 case R.id.radio_dog:
                     imgv.setImageResource(R.drawable.dog);
                     break;
